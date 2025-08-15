75ee5e4add3397ac2d461bb589bf4a811aa86a4e
jenkins_ci <jenkins_ci@limxdynamics.com>
2025-08-15 18:22:14 +0800

    3.0.17.20250815182140

--- ros2/launch/rosbridger.launch.py
+++ ros2/launch/rosbridger.launch.py
@@ -10,25 +10,9 @@ import sys
 
 
 def generate_launch_description():
-    robot_type = os.getenv("ROBOT_TYPE")
-
-    # Check if the ROBOT_TYPE environment variable is set, otherwise exit with an error
-    if not robot_type:
-        print("Error: Please set the ROBOT_TYPE using 'export ROBOT_TYPE=<robot_type>'.")
-        sys.exit(1)
 
     # Robot ip address
     os.environ['MROS_AGENT_IP'] = '10.192.1.2'
-
-    # Get URDF file path
-    urdf_file = PathJoinSubstitution(
-        [FindPackageShare("robot_description"), "pointfoot/" + robot_type + "/urdf", "robot.urdf"]
-    )
-
-    rviz_config_file = get_package_share_directory('robot_visualization') + '/rviz/pointfoot.rviz'
-
-    # Load the URDF file
-    robot_description = Command(["xacro ", urdf_file, " ",])
 
     return LaunchDescription([
         DeclareLaunchArgument(
@@ -49,19 +33,5 @@ def generate_launch_description():
                     "mros2ros_include": LaunchConfiguration("mros2ros_include"),
                 },
             ],
-        ),
-        Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            parameters=[
-                {
-                    "robot_description": robot_description,
-                },
-            ],
-        ),
-        Node(
-            package='rviz2',
-            executable='rviz2',
-            arguments=['-d', rviz_config_file]
         ),
     ])
